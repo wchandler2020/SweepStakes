0e647bc318155088b32b1312e4c5948a67d160e1
wchandler2018 <36997950+wchandler2018@users.noreply.github.com>
2020-02-01 09:01:52 -0600

    initial commit

---
+++ SweepStatesApp/Constant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStatesApp
+{
+    class Constant
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public int registrationNumber;
+
+        public Constant(string firstName, string lastName, string email, int registrationNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.registrationNumber = registrationNumber;
+        }
+    }
+}
---
+++ SweepStatesApp/UserInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStatesApp
+{
+    class UserInterface
+    {
+        public void Greeting()
+        {
+            
+
+            Console.WriteLine("Hello, Welcome to PCH, where you Could Win a Life-Changing cash and prizes!");
+            Console.WriteLine("What is your first name?");
+            string useFIrstName = Console.ReadLine();
+            Console.WriteLine("What is your first name?");
+            string userLastName = Console.ReadLine();
+            Console.WriteLine($"Helo {useFIrstName} {userLastName} please enter an email address?");
+            string userEmail = Console.ReadLine();
+        }
+    }
+}
